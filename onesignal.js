98c8bad3d8cf4c9aabbd4e4531c18d264b946566
Huiceum <yufn0525@gmail.com>
2025-04-29 03:07:00 +0800

    Add files via upload

--- onesignal.js
+++ onesignal.js
@@ -1,4 +1,3 @@
-<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
   window.OneSignalDeferred = window.OneSignalDeferred || [];
   OneSignalDeferred.push(async function(OneSignal) {
     await OneSignal.init({
