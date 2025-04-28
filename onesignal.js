989d030b7246dd29f2c916d66f7e91791d514420
Huiceum <yufn0525@gmail.com>
2025-04-29 02:59:09 +0800

    Add files via upload

---
+++ onesignal.js
@@ -0,0 +1,13 @@
+<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+  window.OneSignalDeferred = window.OneSignalDeferred || [];
+  OneSignalDeferred.push(async function(OneSignal) {
+    await OneSignal.init({
+      appId: "d5610f64-df3e-49d5-8361-4afa83f33240",
+      safari_web_id: "web.onesignal.auto.087eca46-faed-450d-af5b-90e7f525c88c",
+      notifyButton: {
+        enable: false, // 禁用小鈴鐺按鈕
+      },
+      autoRegister: true, // 打開網頁自動請求通知權限
+    });
+  });
+
